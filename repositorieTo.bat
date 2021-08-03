721077af318336da836ce256b9f16d71fc18555a
czainski <38532082+czainski@users.noreply.github.com>
2021-08-03 21:57:27 +0200

    Add files via upload

---
+++ repositorieTo.bat
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "V1"
+git remote add origin https://github.com/czainski/Memory.git
+rem git remote add origin https://github.com/czainski/WebApiOwinCore3BC3.git
+git branch -M main
+git push -u origin main
